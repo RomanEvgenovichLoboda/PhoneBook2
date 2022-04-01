eb5a0d82263a8b06151776c333c14aba6a02bd55
loboda.rom@gmail.com <loboda.rom@gmail.com>
2022-04-01 21:53:25 +0300

    Добавьте файлы проекта.

---
+++ PhoneBook2/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace PhoneBook2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutoPopDelay = 5000;
+            this.toolTip1.InitialDelay = 10;
+            this.toolTip1.IsBalloon = true;
+            this.toolTip1.ReshowDelay = 100;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(46, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 0;
+            this.toolTip1.SetToolTip(this.textBox1, "Name\r\n");
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(46, 142);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 22);
+            this.textBox2.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.textBox2, "Number");
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(46, 96);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(132, 22);
+            this.textBox3.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.textBox3, "LastName");
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(228, 35);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(382, 150);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 237);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Abonent";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ PhoneBook2/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace PhoneBook2
+{
+   
+    public partial class Form1 : Form
+    { 
+        //DataTable table;
+        //int id = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            UpdateTable();
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
+            {
+                SqlConnection conn = new SqlConnection();
+                conn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PhoneBook;Trusted_Connection=True";
+                try
+                {
+                    string FirstName = textBox1.Text;
+                    string LastName = textBox3.Text;
+                    string Phone = textBox2.Text;
+                    string insertString = "INSERT INTO [Telephones] (Name,LastName,TelNumber)" + "VALUES (@Name, @LastName, @TelNumber);";
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(insertString, conn);
+                    cmd.Parameters.AddWithValue("@Name", FirstName);
+                    cmd.Parameters.AddWithValue("@LastName", LastName);
+                    cmd.Parameters.AddWithValue("@TelNumber", Phone);
+                    cmd.ExecuteNonQuery();
+                }
+                finally
+                {
+                    if (conn != null)
+                    {
+                        conn.Close();
+                        UpdateTable();
+                    }
+                }
+            }
+            else MessageBox.Show("The field is empty");
+        }
+        private void UpdateTable()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PhoneBook;Trusted_Connection=True";
+            try
+            {
+                conn.Open();
+                string command = "SELECT * FROM [Telephones]";
+                SqlDataAdapter adapt = new SqlDataAdapter(command, conn);
+                DataSet ds = new DataSet();
+                adapt.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0].DefaultView;
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    table = new DataTable();
+        //    table.Columns.Add("id");
+        //    table.Columns.Add("Name");
+        //    table.Columns.Add("LastName");
+        //    table.Columns.Add("Number");
+        //    using (SqlConnection conn = new SqlConnection(@"Server = (localdb)\MSSQLLocalDB;Database = PhoneBook;Trusted_Connection=True;"))
+        //    {
+        //        conn.Open();
+        //        string command = "SELECT * FROM [Telephones];";
+        //        SqlDataAdapter adapt = new SqlDataAdapter(command, conn);
+        //        DataSet ds = new DataSet();
+        //        adapt.Fill(ds);
+        //        dataGridView1.DataSource = ds;
+        //        id++;
+        //        DataRow row = table.NewRow();
+        //        row[0] = id;
+        //        row[1] = textBox1.Text;
+        //        row[2] = textBox2.Text;
+        //        row[3] = textBox3.Text;
+        //        table.Rows.Add(row);
+        //        SqlCommandBuilder boilder = new SqlCommandBuilder(adapt);
+        //        adapt.Update(ds);
+        //        conn.Close();
+        //    }
+        //}
+    }
+}
